617cba84f11fc4e6a981a4bee8b629cec23ac09f
unknown <kdl75020@gmail.com>
2022-08-25 23:14:27 +0100

    commit 25/08

--- src/DataProvider/DependencyDataProvider.php
+++ src/DataProvider/DependencyDataProvider.php
@@ -3,13 +3,23 @@
 namespace App\DataProvider;
 
 use ApiPlatform\Core\DataProvider\ContextAwareCollectionDataProviderInterface;
+use ApiPlatform\Core\DataProvider\ItemDataProviderInterface;
 use ApiPlatform\Core\DataProvider\RestrictionDataProviderInterface;
+use ApiPlatform\Core\Exception\ResourceClassNotSupportedException;
 use App\Entity\Dependency;
+use Ramsey\Uuid\Uuid;
 
 class DependencyDataProvider implements ContextAwareCollectionDataProviderInterface,
-RestrictionDataProviderInterface
+RestrictionDataProviderInterface, ItemDataProviderInterface
 {
     public function __construct(private string $rootPath) {}
+
+    private function getDependencies(): array
+    {
+        $path = $this->rootPath . '/composer.json';
+        $json = json_decode(file_get_contents($path), true);
+        return $json['require'];
+    }
 
     public function getCollection(string $resourceClass, string $operationName = null, array $context = [])
     {
@@ -25,6 +35,11 @@ RestrictionDataProviderInterface
     public function supports(string $resourceClass, string $operationName = null, array $context = []): bool
     {
         return $resourceClass == Dependency::class;
+    }
+
+    public function getItem(string $resourceClass, $id, string $operationName = null, array $context = [])
+    {
+        //
     }
 
     
